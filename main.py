9e509344c2855d6c52949997927109c34c99b155
Sander Prins <73616e646572@users.noreply.github.com>
2024-05-23 08:57:28 +0000

    Changed folder structure

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import subprocess
 
-# subprocess.run(['python', './scrapethissite.com/1. Simple/1_Simple.py'])
-# subprocess.run(['python', './scrapethissite.com/2. Forms/2_Forms.py'])
-# subprocess.run(['python', './scrapethissite.com/3. AJAX-JavaScript/3_AJAX-JavaScript.py'])
-# subprocess.run(['python', './scrapethissite.com/4. Frames/4_Frames.py'])
-# subprocess.run(['python', './scrapethissite.com/5. Advanced/5A_Headers.py'])
-subprocess.run(['python', './scrapethissite.com/5. Advanced/5B_Login.py'])
+# subprocess.run(['python', './scrapethissite.com/solutions/1_Simple.py'])
+# subprocess.run(['python', './scrapethissite.com/solutions/2_Forms.py'])
+# subprocess.run(['python', './scrapethissite.com/solutions/3_AJAX-JavaScript.py'])
+# subprocess.run(['python', './scrapethissite.com/solutions/4_Frames.py'])
+# subprocess.run(['python', './scrapethissite.com/solutions/5A_Headers.py'])
+# subprocess.run(['python', './scrapethissite.com/solutions/5B_Login.py'])
+# subprocess.run(['python', './scrapethissite.com/solutions/5C_CSRF.py'])
